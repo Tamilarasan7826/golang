776c479b94b031a1728d4f162dde68f12e7132d8
Tamilarasan7826 <tamzilkannan1433@gmail.com>
2024-11-08 06:06:43 -0800

    go code changes

---
+++ main.go
@@ -0,0 +1,67 @@
+// package main
+
+// import "fmt"
+// "gitup/sql"
+
+// func main() {
+// 	// type userdata struct{
+// 	// 	name string
+// 	// 	email string
+// 	// 	age uint
+// 	// 	city string
+// 	// 	mobile string
+// 	// }
+// 	// fmt.Println("hello")
+
+// 	// var data = userdata{
+// 	// 	name:"tamil",
+// 	// 	email:"tamil@gamil.com",
+// 	//     age :23,
+// 	// 	city:"melpalur",
+// 	// 	mobile:"7826056378",
+// 	// }
+// 	// fmt.Println(data)
+
+// 	// data1:=userdata{"kannan","kannan@gmail.com",23,"palur","7865762376"}
+
+// 	// fmt.Println(data1)
+// 	// fmt.Printf("user details %v\n",data1)
+// 	// fmt.Printf("user details %+v",data1)
+
+// 	// variable==================
+
+//     // var name string="tamil"
+
+// 	// fmt.Println(name)
+// 	// fmt.Println("this is my nane %T:",name)
+// 	// fmt.Printf("this is my nane %T\n:",name)
+// 	// fmt.Printf("this is my nane %v\n:",name)
+
+// 	// fname:="kannan"  // this is not worked outside of the function or method
+
+// 	// fmt.println(fname)
+
+// 	// fmt.Println(kannan())
+// // =====================================================
+
+// // db
+
+// 	}
+
+// // func kannan() string  {
+// // 	sname:="only write some thing"  // its worked inside function is well
+// // 	return sname
+// // }
+
+package main
+
+import "fmt"
+
+
+
+func main() {
+   var name=[]string{"tamil","mani","ram"}
+   name=append(name,"vikki","naveen","jeeva")
+   fmt.Printf("this is slice %v\n",name)
+   fmt.Println(name)
+}
